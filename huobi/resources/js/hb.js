2bcdd3c999f6a5098948799f037e92ba12d8db12
Cobra <c8d8z8@gmail.com>
2018-08-15 17:53:26 +0800

    add hb10 cache kline

--- huobi/resources/js/hb.js
+++ huobi/resources/js/hb.js
@@ -2,8 +2,23 @@
     "use strict";
     var $ = window.jQuery;
     // refresh settings
-    $('#refresh-settings').click(function(){
+    $('#refresh-system-settings').click(function(){
         $.getJSON('/order/refresh_settings',function(data){
+          alert(data.status);
+      });
+    });
+
+    $('#refresh-trading-coins-settings').click(function(){
+        $.getJSON('/order/init/symbol',function(data){
+          alert(data.status);
+      });
+    });
+    $('#refresh-kline-history').click(function(){
+        $.getJSON('/kline/init',function(data){
+          alert(data.status);
+      });
+    });$('#start_t_kline_history').click(function(){
+        $.getJSON('/thread/kline/start',function(data){
           alert(data.status);
       });
     });
